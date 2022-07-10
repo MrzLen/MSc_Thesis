8fa5b887890f9c674c3150be38feaaddc73e964d
Len <pan@Pans-MacBook-Pro.local>
2022-07-10 23:00:46 +0100

    little progress

---
+++ bayesian_pdes/__init__.py
@@ -0,0 +1,4 @@
+__author__ = 'benorn'
+from .collocation import collocate
+from . import operator_compilation
+from . import problems
---
+++ bayesian_pdes/operator_compilation/__init__.py
@@ -0,0 +1,5 @@
+from .sympy_compiler import compile_sympy
+from .caching import CachingOpCache
+from .composite import CompositeOperatorSystem
+from . import sympy_gram
+
